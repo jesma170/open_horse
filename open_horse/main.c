77c8b8d7872e35236c50e27e4f462bcf1ea8de89
jesma170 <jesma8506@gmail.com>
2024-08-11 20:36:03 +0200

    Update main.c

--- open_horse/main.c
+++ open_horse/main.c
@@ -5,11 +5,11 @@
 typedef char					s8_t;
 typedef short					s16_t;
 typedef int						s32_t;
-typedef long long				s64_t;
+typedef long long			s64_t;
 
 typedef unsigned char		u8_t;
-typedef unsigned short		u16_t;
-typedef unsigned int			u32_t;
+typedef unsigned short	u16_t;
+typedef unsigned int		u32_t;
 typedef unsigned long long	u64_t;
 
 typedef struct s_actor_t {
